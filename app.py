846e0250ebafe3d28cb38f66701244dc4775d9d8
Shubh <81286735+shubh-3011@users.noreply.github.com>
2024-11-08 09:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,5 +134,6 @@ def view_cart():
         cursor.close()
         conn.close()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
