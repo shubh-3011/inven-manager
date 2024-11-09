a369cbd84c249b0f617b0f990761ecce82a37c95
Shubh <81286735+shubh-3011@users.noreply.github.com>
2024-11-10 00:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,5 @@ def view_cart():
         return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
+
